6f2fde39a4218293403050455732d6e3557ff501
Jean-Baptiste Note <jb.note@criteo.com>
2015-11-26 16:00:11 +0100

    [DISTRO-765] Fix capacity scheduler webui issue

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/CapacitySchedulerPage.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/webapp/CapacitySchedulerPage.java
@@ -120,7 +120,7 @@ class CapacitySchedulerPage extends RmView {
           _("Configured Max Capacity:", percent(lqinfo.getMaxCapacity() / 100)).
           _("Configured Minimum User Limit Percent:", Integer.toString(lqinfo.getUserLimit()) + "%").
           _("Configured User Limit Factor:", String.format("%.1f", lqinfo.getUserLimitFactor())).
-          _("Active Users: ", activeUserList.toString()).
+          _r("Active Users: ", activeUserList.toString()).
           _r("Accessible Node Labels:", StringUtils.join(",", lqinfo.getNodeLabels()));
 
       html._(InfoBlock.class);
